3bb34b92483891ed82eccddc6585a4b18dc5c59f
Gojko Adzic <gojko@gojko.com>
2015-05-07 01:51:05 +0200

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # hexgridwidget
-jQuery Hex Grid Widget
+A simple jQuery Hex Grid Widget, implemented using SVG. Fields can are clickable
+and have meta-data attached to signal row and column position. Everything can be
+styled using CSS
+
---
+++ hexgridwidget.js
@@ -0,0 +1,37 @@
+/*global $, document*/
+$.fn.hexGridWidget = function (radius, columns, rows, cssClass) {
+	'use strict';
+	var createSVG = function (tag) {
+		return $(document.createElementNS('http://www.w3.org/2000/svg', tag || 'svg'));
+	};
+	return $(this).each(function () {
+		var element = $(this),
+				height = Math.sqrt(3) / 2 * radius,
+				svgParent = createSVG('svg').attr('tabindex', 1).appendTo(element).css({
+					width: (1.5 * columns  +  0.5) * radius,
+					height: (2 * rows  +  1) * height
+				}),
+				column, row, center,
+				toPoint = function (dx, dy) {
+					return Math.round(dx + center.x) + ',' + Math.round(dy + center.y);
+				};
+		for (row = 0; row < rows; row++) {
+			for (column = 0; column < columns; column++) {
+				center = {x:(1 + 1.5 * column) * radius, y: height * (1 + row * 2 + (column % 2))};
+				createSVG('polygon').attr({
+					points: [
+						toPoint(-1 * radius / 2, -1 * height),
+						toPoint(radius / 2, -1 * height),
+						toPoint(radius, 0),
+						toPoint(radius / 2, height),
+						toPoint(-1 * radius / 2, height),
+						toPoint(-1 * radius, 0)
+					].join(' '),
+					'class':cssClass,
+					tabindex:1
+				})
+				.appendTo(svgParent).data({center:center, row:row, column:column});
+			}
+		}
+	});
+};
